787440cf5e60d721d316216a12ea711d360bafb3
Vincenzo Ceccarelli Grimaldi <59419763+iceccarelli@users.noreply.github.com>
2026-05-23 19:58:04 +0200

    Add OData module exports for clean imports

---
+++ server/odata/index.ts
@@ -0,0 +1,6 @@
+/**
+ * OData module exports for clean imports.
+ * Main entry for the Microsoft-aligned OData layer.
+ */
+export * from "./router";
+export { odataRouter as default } from "./router";
